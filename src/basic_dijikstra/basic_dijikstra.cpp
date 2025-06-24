cee2271278bc71e3dd8ba3dcae614a901a6fe2f0
Alexander Ivanov <aiirobotics@pm.me>
2025-06-24 04:33:53 +0000

    Writing the basic dijikstra functionality. Have not done major functions

--- src/basic_dijikstra/basic_dijikstra.cpp
+++ src/basic_dijikstra/basic_dijikstra.cpp
@@ -1,6 +1,52 @@
 #include "basic_dijikstra/basic_dijikstra.h"
+#include <cmath>
 
-BasicDijikstra::BasicDijikstra()
+BasicDijikstra::BasicDijikstra(std::shared_ptr<Lattice> lattice, double x_pitch, double y_pitch)
 {
-  temp = std::vector<int>(1, 1);
+  path_.push_back(State());
+  lattice_ = lattice;
+  x_size_ = lattice->size();
+  y_size_ = (*lattice)[0].size();
+  x_pitch_ = x_pitch;
+  y_pitch_ = y_pitch;
+}
+
+bool BasicDijikstra::SetStart(State start){
+  bool same_start = start==start_;
+  start_ = start;
+  return same_start;
+}
+
+bool BasicDijikstra::SetGoal(State goal){
+  bool same_goal = goal==goal_;
+  goal_=goal;
+  return same_goal;
+}
+
+bool BasicDijikstra::DoSearch(){
+  //Todo implement this
+  return false;
+}
+
+std::vector<BasicDijikstra::NodePtr> BasicDijikstra::GetNeighbors(NodePtr node){
+   std::vector<NodePtr> neighbours;
+  //Todo Implement this
+  return neighbours;
+}
+
+BasicDijikstra::NodePtr BasicDijikstra::GetNearest(State query){
+  double x_idx_; 
+  double y_idx_; 
+
+  double x_remain = std::modf(query.x_pos/x_pitch_, &x_idx_);
+  double y_remain = std::modf(query.y_pos/y_pitch_, &y_idx_);
+
+  if(x_remain>0.5){
+    x_idx_++;
+  }
+  if(y_remain>0.5){
+    y_idx_++;
+  }
+
+  return std::make_shared<SearchNode>((*lattice_)[(int)x_idx_][(int)y_idx_]);
 }
--- src/basic_dijikstra/include/basic_dijikstra/basic_dijikstra.h
+++ src/basic_dijikstra/include/basic_dijikstra/basic_dijikstra.h
@@ -1,10 +1,41 @@
 #include <vector>
 #include <memory>
+#include <queue>
+#include "search_node.h"
+
+struct State{
+  double x_pos = 0;
+  double y_pos = 0;
+  double angle = 0;
+  friend bool operator==(const State& l, const State& r) noexcept{
+    return (l.x_pos==r.x_pos) && (l.y_pos==r.y_pos)&& (l.angle==r.angle);
+  }
+};
+
 
 class BasicDijikstra {
 public:
-  BasicDijikstra();
+  using Path = std::vector<State>;
+  using Lattice = std::vector<std::vector<SearchNode>>;
+  using NodePtr = std::shared_ptr<SearchNode>;
+
+  BasicDijikstra(std::shared_ptr<Lattice> lattice, double x_pitch, double y_pitch);
+
+  bool SetStart(State start);
+  bool SetGoal(State goal);
+  bool DoSearch();
+  NodePtr GetNearest(State query);
+  std::vector<NodePtr> GetNeighbors(NodePtr node);
+
 private:
-  std::vector < int > temp;
+  Path path_ = Path();
+  std::priority_queue<NodePtr> queue_; 
+  std::shared_ptr<Lattice> lattice_;
+  State start_ = State();
+  State goal_ = State();
+  size_t x_size_ = 0;
+  size_t y_size_ = 0;
+  double x_pitch_ =0;
+  double y_pitch_ =0;
 
 };
